9455fe883d836590f52efa26e2b034bdfe095594
dk-ksd <mrdeekshithk@gmail.com>
2025-08-25 23:23:29 +0530

    Updated render-build.sh

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -2,13 +2,9 @@
 # exit on error
 set -o errexit
 
-# Install system dependencies
-apt-get update
-apt-get install -y ffmpeg
-
 # Install Python dependencies
 pip install -r requirements.txt
 
 # Run Django management commands from backend/
-python backend/manage.py collectstatic --noinput
-python backend/manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py migrate
